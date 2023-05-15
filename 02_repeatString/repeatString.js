74ccd9d3ae9c2e5105f0daa416eae5168ef0ef50
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-15 18:52:29 -0400

    Add repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
-const repeatString = function(word, times) {
+const repeatString = function (word, times) {
  if (times < 0) return "ERROR";
  let string = "";
- for (let i = 0; i < times; i++){
+ for (let i = 0; i < times; i++) {
     string += word;
  }
  return string;
