6d88483167d12974f2c85774e1338222d0bd7ddb
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-23 21:05:30 -0400

    Add 11_getTheTitles/getTheTitles.spec.js

--- 11_getTheTitles/getTheTitles.spec.js
+++ 11_getTheTitles/getTheTitles.spec.js
@@ -1,16 +1,16 @@
-const getTheTitles = require('./getTheTitles')
+const getTheTitles = require('./getTheTitles');
 
 describe('getTheTitles', () => {
     const books = [
       {
         title: 'Book',
-        author: 'Name'
+        author: 'Name',
       },
       {
         title: 'Book2',
-        author: 'Name2'
-      }
-    ]
+        author: 'Name2',
+      },
+    ];
 
   test('gets titles', () => {
     expect(getTheTitles(books)).toEqual(['Book','Book2']);
